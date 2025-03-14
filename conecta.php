c97cbc8e4a13d431d80da32d454910720f3cfd67
Vanessa Baldin <vanbaldin08@gmail.com>
2025-03-14 10:02:42 -0300

    Configura PDO

---
+++ conecta.php
@@ -0,0 +1,21 @@
+<php
+
+/* Parâmetros de conexão em ambiente de desenvolvimento, no nosso caso, o XAMPP */
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "vendas";
+
+/* Configurações para conexão ao banco de dados*/
+
+// Criando conexão com o banco usando a classe PDO
+// PDO (PHP Data Object): classe para munipulação de banco de dados
+
+$conexao = new PDO(
+  "mysql:host=$servidor;dbname=$banco;charset=utf8",
+   $usuario, $senha
+
+);
+
+
