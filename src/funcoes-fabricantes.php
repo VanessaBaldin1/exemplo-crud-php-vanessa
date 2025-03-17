15e962517d3cc618ea22164a291351731a23788b
Vanessa Baldin <vanbaldin08@gmail.com>
2025-03-17 09:51:20 -0300

    Adiciona função de Inserir fabricante

--- src/funcoes-fabricantes.php
+++ src/funcoes-fabricantes.php
@@ -26,6 +26,22 @@ function listarFabricantes(PDO $conexao): array
 }
 
 // inserirFabricante: usada pela página fabricante/inserir.php
-function inserirFabricante(PDO $conexao, string $nomeDoFabricante){
-  "INSERT "
+function inserirFabricante(PDO $conexao, string $nomeDoFabricante):void{ //void indica que não tem retorno da função
+  
+  /*: named parameter (parâmetro nomeado) 
+  Usamos este recurso do PDO para 'reservar' um espaço seguro em memória para colocação do dado. Nunca passe de forma direta valores para comandos SQL*/  
+  $sql = "INSERT INTO fabricantes(nome) VALUES(:nome)";
+
+  try {
+    $consulta = $conexao->prepare($sql);
+
+    /* bindValue() -> permite vincular o valor
+    do parâmetro à consulta que será executada.É necessário indicar qual é o (:nome), de onde vem o valor ($nomeDoFabricante) e de que tipo ele é (PDO:PARAM_STR)*/ 
+    $consulta->bindValue(":nome", $nomeDoFabricante, PDO::PARAM_STR);
+
+
+    $consulta->execute();
+  } catch (Exception $erro) {
+    die("Erro ao inserir: " .$erro->getMessage());
+  }
 }
