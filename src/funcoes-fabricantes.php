97d638702d449d0ccd7be4bb98b6c3cf0cdee09b
Vanessa Baldin <vanbaldin08@gmail.com>
2025-03-17 09:16:54 -0300

    refatora funcoes -fabricantes try e catch

--- src/funcoes-fabricantes.php
+++ src/funcoes-fabricantes.php
@@ -1,21 +1,26 @@
 <?php
 require_once "conecta.php";
 
-/* Lógica/funções para o CRUD de Fabricantes*/ 
+/* Lógica/funções para o CRUD de Fabricantes*/
 
 //listarFabricantes: usada pela página fabricantes/visualizar.php
 
-function listarFabricantes(PDO $conexao):array {
+function listarFabricantes(PDO $conexao): array
+{
   $sql = "SELECT * FROM fabricantes ORDER BY nome";
 
-  /* Preparando o comando SQL ANTES de executar no servidor e guardando em memória (variável consulta ou query) */
-  $consulta = $conexao->prepare($sql);
+  try {
+    /* Preparando o comando SQL ANTES de executar no servidor e guardando em memória (variável consulta ou query) */
+    $consulta = $conexao->prepare($sql);
 
-  /* Executando o comando no banco de dados*/
+    /* Executando o comando no banco de dados*/
 
-  $consulta->execute();
+    $consulta->execute();
 
-  /* "fetch All", Busca/Retorna todos os dados provenientes da execução da consulta e os transforma em um array associativo */
-  return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    /* "fetch All", Busca/Retorna todos os dados provenientes da execução da consulta e os transforma em um array associativo */
+    return $consulta->fetchAll(PDO::FETCH_ASSOC);
 
+  } catch (Exception $erro) {
+     die("Erro: ".$erro-> getMessage());
+  }
 }
